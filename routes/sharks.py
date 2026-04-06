060ae0258968301ba083ec0a877118dc67f88d20
durabass856 <durabass856@gmail.com>
2026-04-06 21:30:28 +0530

    Update sharks.py

--- routes/sharks.py
+++ routes/sharks.py
@@ -12,54 +12,16 @@ def get_all():
         ORDER BY sh.shark_id DESC
     """
     data, err = query(sql)
-    if err: return error(err)
-    return success(data)
+    return success(data) if not err else error(err)
 
 @sharks_bp.route("/<int:sid>", methods=["GET"])
 def get_one(sid):
     shark, err = query("SELECT * FROM shark WHERE shark_id=%s", (sid,), fetch="one")
+
     if err: return error(err)
     if not shark: return error("Shark not found", 404)
+
     expertise, _ = query("SELECT * FROM shark_expertise WHERE shark_id=%s", (sid,))
     shark["expertise"] = expertise or []
+
     return success(shark)
-
-@sharks_bp.route("/", methods=["POST"])
-def create():
-    b = request.get_json()
-    if not b or not b.get("first_name"):
-        return error("first_name is required")
-    sql = """INSERT INTO shark
-             (first_name,last_name,email,phone,date_of_birth,nationality,
-              net_worth_usd_millions,company_id,bio)
-             VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-    company_id = b.get("company_id") or None
-    args = (b.get("first_name"),b.get("last_name"),b.get("email"),b.get("phone"),
-            b.get("date_of_birth"),b.get("nationality"),b.get("net_worth_usd_millions"),
-            b.get("company_id"),b.get("bio"))
-    data, err = query(sql, args, fetch="none")
-    if err: return error(err)
-    sid = data["lastrowid"]
-    if b.get("expertise_domain"):
-        query("INSERT INTO shark_expertise (shark_id,domain,is_primary) VALUES (%s,%s,1)",
-              (sid, b["expertise_domain"]), fetch="none")
-    return success({"shark_id": sid}, "Investor created", 201)
-
-@sharks_bp.route("/<int:sid>", methods=["PUT"])
-def update(sid):
-    b = request.get_json()
-    sql = """UPDATE shark SET first_name=%s,last_name=%s,email=%s,phone=%s,
-             nationality=%s,net_worth_usd_millions=%s,company_id=%s,bio=%s
-             WHERE shark_id=%s"""
-    args = (b.get("first_name"),b.get("last_name"),b.get("email"),b.get("phone"),
-            b.get("nationality"),b.get("net_worth_usd_millions"),b.get("company_id"),
-            b.get("bio"),sid)
-    data, err = query(sql, args, fetch="none")
-    if err: return error(err)
-    return success(message="Investor updated")
-
-@sharks_bp.route("/<int:sid>", methods=["DELETE"])
-def delete(sid):
-    data, err = query("DELETE FROM shark WHERE shark_id=%s", (sid,), fetch="none")
-    if err: return error(err)
-    return success(message="Investor deleted")
